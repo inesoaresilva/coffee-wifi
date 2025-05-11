ed68ea0348d8414092043ccbcf4420eaa9750d5d
Inês Soares Silva <inesoaresilva@gmail.com>
2025-05-11 18:23:46 +0100

    data: add image type and images to cafe data

--- src/types/cafe.ts
+++ src/types/cafe.ts
@@ -1,19 +1,22 @@
+import { Image } from './image'
+
 export interface Cafe {
-    name: string,
-    slug: string,
-    imageUrl: string,
-    neighbourhood: string,
-    wifi: {
-        speed: string,
-        uploadSpeed: string,
-        downloadSpeed: string,
-    },
-    socketsAvailable: string,
-    noiseLevel: string,
-    comfort: string,
-    light: string,
-    tags: string[],
-    pricePoint: number,
-    lastVisit: string,
-    review: string
+  name: string
+  slug: string
+  imageUrl: string
+  images: Image[]
+  neighbourhood: string
+  wifi: {
+    speed: string
+    uploadSpeed: string
+    downloadSpeed: string
   }
+  socketsAvailable: string
+  noiseLevel: string
+  comfort: string
+  light: string
+  tags: string[]
+  pricePoint: number
+  lastVisit: string
+  review: string
+}
