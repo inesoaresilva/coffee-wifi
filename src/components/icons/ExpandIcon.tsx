20c1bfb1aa8e6c4dbb7352b12453efede13f6822
Inês Soares Silva <inesoaresilva@gmail.com>
2025-05-21 15:06:00 +0100

    chore: add icons

---
+++ src/components/icons/ExpandIcon.tsx
@@ -0,0 +1,16 @@
+const ExpandIcon = () => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M0 20V11.4286H2.85714V17.1429H8.57143V20H0ZM17.1429 8.57143V2.85714H11.4286V0H20V8.57143H17.1429Z"
+      fill="#2C2107"
+    />
+  </svg>
+)
+
+export default ExpandIcon
