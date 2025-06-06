f9496cdc1979ea7e70a47c6e02e36f91de858760
Inês Soares Silva <inesoaresilva@gmail.com>
2025-06-06 16:02:50 +0100

    refactor: update CafeView component

--- src/app/cafes/[cafeName]/page.tsx
+++ src/app/cafes/[cafeName]/page.tsx
@@ -2,16 +2,21 @@ import CafeView from '@/components/CafeView'
 import { getCafeBySlug } from '@/helpers/getCafeBySlug'
 
 type Props = {
-  params: Promise<{
+  params: {
     cafeName: string
-  }>
+  }
 }
 
 async function name({ params }: Props) {
-  const cafe = getCafeBySlug((await params).cafeName)
-  if (cafe) {
-    return <CafeView cafe={cafe}></CafeView>
-  }
+  const cafe = getCafeBySlug(params.cafeName)
+
+  if (!cafe) return null
+
+  return (
+    <main>
+      <CafeView cafe={cafe} />
+    </main>
+  )
 }
 
 export default name
--- src/components/CafeView.tsx
+++ src/components/CafeView.tsx
@@ -1,6 +1,8 @@
 import { Cafe } from '@/types/cafe'
 import styles from './CafeView.module.css'
 import ImageGallery from './ImageGallery'
+import Link from 'next/link'
+import ArrowLeftIcon from './icons/ArrowLeftIcon'
 
 type CafeViewProps = {
   cafe: Cafe
@@ -9,7 +11,14 @@ type CafeViewProps = {
 function CafeView({ cafe }: CafeViewProps) {
   return (
     <>
-      <h2>{cafe.name}</h2>
+      <Link
+        href="/"
+        className={styles.nameWrapper}
+        aria-label={`Go back to the main page`}
+      >
+        <h2 className={styles.name}>{cafe.name}</h2>
+        <ArrowLeftIcon color={'#2c2107'} />
+      </Link>
       <p className={styles.review}>{cafe.review}</p>
       {cafe.images?.length > 0 && <ImageGallery images={cafe.images} />}
     </>
